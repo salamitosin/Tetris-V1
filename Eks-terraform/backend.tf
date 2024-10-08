1d6610ddbbec36f64162b4707eb1a8922cbfd624
salamitosin <81862571+salamitosin@users.noreply.github.com>
2024-10-08 23:51:33 +0100

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "terraboket" # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }

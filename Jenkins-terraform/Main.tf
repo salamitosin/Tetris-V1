0aa53eb2942131d56c8f3d9efe4503b873d71e5d
salamitosin <81862571+salamitosin@users.noreply.github.com>
2024-10-08 23:25:18 +0100

    updated
    j

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,9 +59,9 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0df4b2961410d4cff"
-  instance_type          = "t2.medium"
-  key_name               = "mumbai-mac"
+  ami                    = "ami-005fc0f236362e99f"
+  instance_type          = "t2.large"
+  key_name               = "csn-key-pair"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
